e1bd80521ea4eeffc058174b4a9dbf211c9c6b70
Lisbon <lisbon@dublin.com>
2023-06-12 18:40:33 +0300

    Vars update!

--- codewars/you-only-need-one.js
+++ codewars/you-only-need-one.js
@@ -57,12 +57,62 @@ const check7 = (a, x) =>
 
 const check8 = (array, value) => !!~array.indexOf(value);
 
-
-// var is used within the scope of a function, 
-// but may match same variable withing the windows 
+// used to be one key variable in js, which was var
+// let and const is a block variable;
+// var is used within the scope of a function, as with loops, but not outside!
+// but may match same variable within the windows 
 // not the best usage! We don't know further use of var variable in coding! 
 // if matched further, it can override and cause issues! 
 // In this closed excample, it's OK to use var!
+
+// minuses: allows var variables to redeclare multiple times, when not intended!
+// let and const won't ever let us do this!
+
+/*
+Exg. 
+if(true) {
+    varVarVariable = 'This is not true';
+}
+
+// outside function:
+    varVariable= = 'This is false';
+
+console.log(varVarriable);
+
+let letVariable = 'true';
+let letVariable = 'false';
+
+console.log(letVariable);
+
+Exg. Var will allow, let and const won't let it! 
+
+console.log(varVariable);
+
+var varvariable = 'true';  // undefined 
+
+console.log(varVariable); // here will not let change value!
+
+// const allows to change the actual properties,
+// but not the values of reaassigning the variable to a diferent value,
+// but not actually changing the different values in that property! 
+
+egz. :
+
+const constVar = {name: 'Bob'}
+let letVar = 1;
+
+constVar.name = 'Sally';
+console.log(constVar);
+
+Consclutsion: use const over let and over var!
+almost never use var, better change your design and use let and const!
+
+
+
+
+*/
+
+
 function check9(a, x) {
     let check = false
     for(var j in a){
