508455efb91cc7352200cf5024ebabc0b231c560
Lisbon <lisbon@dublin.com>
2023-06-12 21:19:35 +0300

    Abriviate a two word  prrep.

--- codewars/multiplication-table-for-number.js
+++ codewars/multiplication-table-for-number.js
@@ -1,8 +1,26 @@
 console.clear();
 
+ // to be reviewed... 
 function multiTable(number) {
-    // good luck
   }
 
 console.log(multiTable(5), '1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50');
 console.log(multiTable(1), '1 * 1 = 1\n2 * 1 = 2\n3 * 1 = 3\n4 * 1 = 4\n5 * 1 = 5\n6 * 1 = 6\n7 * 1 = 7\n8 * 1 = 8\n9 * 1 = 9\n10 * 1 = 10');
+
+
+const multiplier = 5;
+const n1 = 1;
+const n2 = 2;
+const n3 = 3;
+const n4 = 4;
+const n5 = 5;
+const n6 = 6;
+const n7 = 7;
+const n8 = 8;
+const n9 = 9;
+const n10 = 10;
+
+const lentele1 = multiplier + ' * ' + n1 + ' = ' + (multiplier * n1) + '\r\n' + multiplier + ' * ' + n2 + ' = ' + (multiplier * n2) + '\r\n' + multiplier + ' * ' + n3 + ' = ' + (multiplier * n3) + '\r\n' + multiplier + ' * ' + n4 + ' = ' + (multiplier * n4) + '\r\n' + multiplier + ' * ' + n5 + ' = ' + (multiplier * n5) + '\r\n' + multiplier + ' * ' + n6 + ' = ' + (multiplier * n6) + '\r\n' + multiplier + ' * ' + n7 + ' = ' + (multiplier * n7) + '\r\n' + multiplier + ' * ' + n8 + ' = ' + (multiplier * n8) + '\r\n' + multiplier + ' * ' + n9 + ' = ' + (multiplier * n9) + '\r\n' + multiplier + ' * ' + n10 + ' = ' + (multiplier * n10);
+
+console.log(multiTable(5));
+console.log(multiTable(2));
