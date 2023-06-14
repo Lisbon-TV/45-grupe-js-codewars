6717adbd8bd1b91fc0d48c036de0b5ff0219d9dd
Lisbon <lisbon@dublin.com>
2023-06-14 11:30:10 +0300

    Devisible by givin  number

---
+++ codewars/find-nunbers-that-are-divisible-by-given-number.js
@@ -0,0 +1,13 @@
+console.clear();
+let numbers = [1, 2, 3, 4, 5, 6];
+let divisor = 2;
+
+function divisibleBy(numbers, divisor){
+    return numbers.filter((number) => number % divisor === 0)
+}
+
+console.log(divisibleBy([1,2,3,4,5,6], 2), [2,4,6]);
+console.log(divisibleBy([1,2,3,4,5,6], 3), [3,6]);
+console.log(divisibleBy([0,1,2,3,4,5,6], 4), [0,4]);
+console.log(divisibleBy([0], 4), [0]);
+console.log(divisibleBy([1,3,5], 2), []);
