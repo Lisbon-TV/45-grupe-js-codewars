2acd92b8a4bac9b41d6ee25df30cf2d4ecdc7d7e
Lisbon <lisbon@dublin.com>
2023-06-15 16:51:39 +0300

    Array.diff solutions

--- codewars/array.diff.js
+++ codewars/array.diff.js
@@ -23,3 +23,129 @@ console.log(arrayDiff([1,2,3], [1,2]), [3], "a was [1,2,3], b was [1,2]")
 // }
 // difference(arr1, arr2)
 
+
+//Other solutions:
+
+// function arrayDiff(a, b) {
+//     if(b.length==0 || a.length==0)
+//       return a;
+//     for(let i =0;i<a.length;i++){
+//       for(let j=0;j<b.length;j++){
+//         if(a[i]==b[j]){
+//           a.splice(i,1);
+//           i--;
+//         }
+//       }
+//     }
+//     return a;
+//   }
+
+
+//   function arrayDiff(a, b) {
+//     return a.filter(x => !b.includes(x));
+//   }
+
+//   var array_diff = (a, b) => a.filter(item => b.indexOf(item) < 0)
+
+//   function array_diff(a, b) {
+//     return a.filter(item => !b.includes(item));
+//   }
+
+
+//   function arrayDiff(a, b) {
+//     // PREP
+//     // (P)arameters - takes in 2 arrays
+//     // (R)eturn - returns an array a without
+//     // anything in array b
+//     // (E)xamples - 
+//     //  ([1, 2], [1]) => [2]
+//     // ([1, 2, 2], [2]) => [1]
+//     // (P)seudocode - 
+//     // Create new array
+//     let newArr = []
+//     // Loop through array a
+//     for (let i = 0; i < a.length; i++) {
+//       // Only push elements from a that
+//       // aren't in b
+//       if (!b.includes(a[i])) {
+//         newArr.push(a[i])
+//       }
+//     }
+//     return newArr
+//   }
+
+//   function array_diff(a, b) {
+//     return a.filter(function (x) {
+//         var bool = true;
+//       b.forEach(function(y) {
+//           if (x == y) bool = false; 
+//       });
+//       return bool;
+//     });
+//   }
+
+
+//   function arrayDiff(a, b) {
+//     b.forEach(
+//       value_in_b => a = a.filter(
+//         vaulue_in_a=>vaulue_in_a!==value_in_b
+//       )
+//     );
+//     return a    
+//   }
+
+//   function arrayDiff(a, b) {
+//     function duplicate(c) {
+//       let cUnique = [];
+//       c.forEach(element => {
+//         if (!cUnique.includes(element)) {
+//           cUnique.push(element)
+//         }
+//       });
+//       return cUnique
+//     }
+  
+//     if (a.length ===  0 ){
+  
+//      return a
+  
+//    } else if ( b.length === 0 ) {
+  
+//       return a
+  
+//     } else {
+  
+//       let x = a
+//       let y = b
+  
+    
+  
+//       return x.filter(n => y.indexOf(n) === -1);
+//     }
+  
+//   }
+
+//   function arrayDiff(a, b) {
+//     for (let i = 0; i < a.length; i++) {
+//         for (let j = 0; j < b.length; j++) {
+//             if (a[i] === b[j]) {
+//                 a.splice(i, 1);
+//                 i--;
+//             }
+//         }
+//     }
+//     return a;
+// }
+
+// function array_diff(a, b) {
+//     return a.filter(function(value)
+//     {
+//       return !(b.includes(value))  
+//     });
+//   }
+
+
+//   function array_diff(a, b) {
+//     return a.filter(function(v){ return b.indexOf(v) < 0 })  
+//   }
+
