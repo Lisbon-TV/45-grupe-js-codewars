c26709366c107df9c469ae9baf98cdcb9908ca3e
Lisbon <lisbon@dublin.com>
2023-06-19 11:29:18 +0300

    You Can't Code Under Pressure #1 + Solution

---
+++ codewars/you-can't-code-under-pressure-#1.js
@@ -0,0 +1,13 @@
+console.clear();
+
+/*
+Description:
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+function doubleInteger(int) {
+    // i will be an integer. Double it and return it.
+    return int * 2;
+  }
+
+console.log(doubleInteger(2), 4);  
