8decc2dd6d98846b1434084d1ef56bfdd80f4aee
Lisbon <lisbon@dublin.com>
2023-06-19 11:32:35 +0300

    You can't code under pressure #1 - Other Solutions

--- codewars/you-can't-code-under-pressure-#1.js
+++ codewars/you-can't-code-under-pressure-#1.js
@@ -11,3 +11,33 @@ function doubleInteger(int) {
   }
 
 console.log(doubleInteger(2), 4);  
+
+
+// Other Solutions: 
+
+
+const doubleInteger = i => 2 * i;
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i+i;
+  }
+
+
+  unction doubleInteger(i) {
+    return i << 1;
+  }
+  
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return parseInt(i) * 2;
+}  
+
+function doubleInteger(i) {
+    if (i === 0) {
+    return 0;
+    } else {
+      return i *= 2;
+     }
+      return i;
+    }
